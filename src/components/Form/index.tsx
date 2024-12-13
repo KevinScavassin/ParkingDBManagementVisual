9db2b9d6a8681304616caa5b18c2f367e9960d84
Kevin <kevinscavassin@gmail.com>
2024-12-13 01:07:21 -0300

    Changed Endpoint name

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -34,14 +34,12 @@ const Form: React.FC<FormProps> = ({ selectedTable }) => {
         return 'https://localhost:7162/api/Endereco';
       case 'estacionamentos':
         return 'https://localhost:7162/api/Estacionamento';
-      case 'tiposveiculos':
-        return 'https://localhost:7162/api/tiposveiculos';
       case 'telefones':
         return 'https://localhost:7162/api/Telefone';
       case 'reservas':
         return 'https://localhost:7162/api/Reserva'; 
       case 'veiculos':
-        return 'https://localhost:7162/api/Veiculos'; 
+        return 'https://localhost:7162/api/Veiculo'; 
       case 'tipoveiculo':
         return 'https://localhost:7162/api/TipoVeiculo'; 
       case 'estadosbrasileiros':
@@ -82,65 +80,108 @@ const Form: React.FC<FormProps> = ({ selectedTable }) => {
           <>
             <input type="text" name="Nome" placeholder="Nome do Cliente" onChange={handleChange} />
             <input type="text" name="CPF" placeholder="CPF" onChange={handleChange} />
+            <input type="text" name="CNPJ" placeholder="CNPJ" onChange={handleChange} />
           </>
         );
       case 'pagamentos':
         return (
           <>
-            <input type="number" name="IDMetodo" placeholder="ID do Método de Pagamento" onChange={handleChange} />
-            <input type="number" name="IDEntradaSaida" placeholder="ID de Entrada/Saída" onChange={handleChange} />
-            <input type="number" name="IDCliente" placeholder="ID do Cliente" onChange={handleChange} />
             <input type="number" name="Valor" placeholder="Valor do Pagamento" onChange={handleChange} />
+            <input type="number" name="MetodoPagamentoId" placeholder="ID do Método de Pagamento" onChange={handleChange} />
+            <input type="number" name="ClienteId" placeholder="ID do Cliente" onChange={handleChange} />
+          </>
+        );
+      case 'metodospagamento':
+        return (
+          <>
+            <input type="text" name="Descricao" placeholder="Descrição do Método de Pagamento" onChange={handleChange} />
           </>
         );
       case 'vagas':
         return (
           <>
-            <input type="number" name="IDEstacionamento" placeholder="ID do Estacionamento" onChange={handleChange} />
-            <input type="number" name="IDTipoVeiculo" placeholder="ID do Tipo de Veículo" onChange={handleChange} />
-            <input type="number" name="IDStatusVaga" placeholder="ID do Status da Vaga" onChange={handleChange} />
+            <input type="number" name="EstacionamentoId" placeholder="ID do Estacionamento" onChange={handleChange} />
+            <input type="number" name="TipoVeiculoId" placeholder="ID do Tipo de Veículo" onChange={handleChange} />
+            <input type="number" name="StatusVagaId" placeholder="ID do Status da Vaga" onChange={handleChange} />
+          </>
+        );
+      case 'enderecos':
+        return (
+          <>
+            <input type="text" name="Rua" placeholder="Rua" onChange={handleChange} />
+            <input type="text" name="Cidade" placeholder="Cidade" onChange={handleChange} />
+            <input type="number" name="EstadoId" placeholder="ID do Estado" onChange={handleChange} />
           </>
         );
       case 'telefones':
         return (
           <>
-            <input type="number" name="IDCliente" placeholder="ID do Cliente" onChange={handleChange} />
+            <input type="text" name="DDD" placeholder="DDD" onChange={handleChange} />
             <input type="text" name="NumeroTelefone" placeholder="Número de Telefone" onChange={handleChange} />
+            <input type="number" name="ClienteId" placeholder="ID do Cliente" onChange={handleChange} />
           </>
         );
       case 'reservas':
         return (
           <>
-            <input type="number" name="IDCliente" placeholder="ID do Cliente" onChange={handleChange} />
-            <input type="number" name="IDVaga" placeholder="ID da Vaga" onChange={handleChange} />
-            <input type="number" name="IDStatusReserva" placeholder="ID do Status da Reserva" onChange={handleChange} />
+            <input type="number" name="ClienteId" placeholder="ID do Cliente" onChange={handleChange} />
+            <input type="number" name="VagaId" placeholder="ID da Vaga" onChange={handleChange} />
+            <input type="number" name="StatusReservaId" placeholder="ID do Status da Reserva" onChange={handleChange} />
+            <input type="datetime-local" name="DataHoraReserva" placeholder="Data e Hora da Reserva" onChange={handleChange} />
           </>
         );
-      case 'veiculos':
+      case 'statusvaga':
         return (
           <>
-            <input type="number" name="IDCliente" placeholder="ID do Cliente" onChange={handleChange} />
-            <input type="text" name="Placa" placeholder="Placa do Veículo" onChange={handleChange} />
-            <input type="text" name="Cor" placeholder="Cor do Veículo" onChange={handleChange} />
+            <input type="text" name="Descricao" placeholder="Descrição do Status da Vaga" onChange={handleChange} />
+          </>
+        );
+      case 'statusreserva':
+        return (
+          <>
+            <input type="text" name="Descricao" placeholder="Descrição do Status da Reserva" onChange={handleChange} />
           </>
         );
       case 'entradassaidas':
         return (
           <>
-            <input type="datetime-local" name="Entrada" placeholder="Data e Hora de Entrada" onChange={handleChange} />
-            <input type="datetime-local" name="Saida" placeholder="Data e Hora de Saída" onChange={handleChange} />
-            <input type="number" name="IDVaga" placeholder="ID da Vaga" onChange={handleChange} />
-            <input type="number" name="IDVeiculo" placeholder="ID do Veículo" onChange={handleChange} />
-            <input type="number" name="IDCliente" placeholder="ID do Cliente" onChange={handleChange} />
-            <input type="number" name="IDPagamento" placeholder="ID do Pagamento" onChange={handleChange} />
+            <input type="datetime-local" name="DataHoraEntrada" placeholder="Data e Hora de Entrada" onChange={handleChange} />
+            <input type="datetime-local" name="DataHoraSaida" placeholder="Data e Hora de Saída" onChange={handleChange} />
+            <input type="number" name="VagaId" placeholder="ID da Vaga" onChange={handleChange} />
+            <input type="number" name="VeiculoId" placeholder="ID do Veículo" onChange={handleChange} />
+            <input type="number" name="ClienteId" placeholder="ID do Cliente" onChange={handleChange} />
+            <input type="number" name="PagamentoId" placeholder="ID do Pagamento" onChange={handleChange} />
+          </>
+        );
+      case 'veiculos':
+        return (
+          <>
+            <input type="text" name="Placa" placeholder="Placa do Veículo" onChange={handleChange} />
+            <input type="text" name="Cor" placeholder="Cor do Veículo" onChange={handleChange} />
+            <input type="text" name="Modelo" placeholder="Modelo do Veículo" onChange={handleChange} />
+            <input type="number" name="ClienteId" placeholder="ID do Cliente" onChange={handleChange} />
+            <input type="number" name="TipoVeiculoId" placeholder="ID do Tipo de Veículo" onChange={handleChange} />
+          </>
+        );
+      case 'tipoveiculo':
+        return (
+          <>
+            <input type="text" name="Descricao" placeholder="Descrição do Tipo de Veículo" onChange={handleChange} />
+          </>
+        );
+      case 'estadosbrasileiros':
+        return (
+          <>
+            <input type="text" name="UF" placeholder="UF do Estado" onChange={handleChange} />
+            <input type="text" name="Nome" placeholder="Nome do Estado" onChange={handleChange} />
           </>
         );
       case 'estacionamentos':
         return (
           <>
             <input type="text" name="Nome" placeholder="Nome do Estacionamento" onChange={handleChange} />
-            <input type="text" name="CapacidadeTotal" placeholder="Capacidade Total" onChange={handleChange} />
-            <input type="number" name="IDEndereco" placeholder="ID do Endereço" onChange={handleChange} />
+            <input type="number" name="Capacidade" placeholder="Capacidade Total" onChange={handleChange} />
+            <input type="number" name="EnderecoId" placeholder="ID do Endereço" onChange={handleChange} />
           </>
         );
       default:
